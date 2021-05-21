b27aaa7ec3a474f6341d792ab9abeadc7ba8e85e
David Moulton <davidmm844 [at] gmail.com>
2021-05-21 16:34:38 +0100

    Only allow dragging from drag handles

--- filters.js
+++ filters.js
@@ -78,13 +78,15 @@ const availableFilters = {
 
   const buildUserFilter = (name, min, max, step, value, image, filters, canvas) => {
     
-    const filter = buildElement('fieldset', { id: `filter_${name}`, draggable: 'true' }, 'filter');
+    const filter = buildElement('fieldset', { id: `filter_${name}` }, 'filter');
 
     const userFilterWrapper = buildElement('div', {}, 'filter__toggle');
     const userFilterLabel = buildElement('label', { for: name }, 'filter__label');
     const userFilterToggle = buildElement('input', { id: name, type: 'checkbox', name: 'filters', value: 'on' }, 'visually-hidden');
     userFilterToggle.addEventListener('input', () => { update(image, filters, canvas); })
     const dragHandle = buildElement('button', { type: 'button' }, 'filter__drag_handle');
+    dragHandle.addEventListener('mousedown', () => { filter.setAttribute('draggable', 'true') });
+    dragHandle.addEventListener('mouseup', () => { filter.removeAttribute('draggable') });
     dragHandle.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" data-filter-move-icon><path d="M0 0h24v24H0z" fill="none"/><path d="M10 9h4V6h3l-5-5-5 5h3v3zm-1 1H6V7l-5 5 5 5v-3h3v-4zm14 2l-5-5v3h-3v4h3v3l5-5zm-9 3h-4v3H7l5 5 5-5h-3v-3z"/></svg>';
 
     const magnitudeWrapper = buildElement('div', {}, 'filter__slider');
@@ -368,6 +370,12 @@ const availableFilters = {
       });
     };
 
+    const prohibitAllFilterDrags = () => {
+      [...form.querySelectorAll('[draggable]')].forEach((draggable) => {
+        draggable.removeAttribute('draggable');
+      });
+    };
+
     const handleFilterDragStart = (e) => {
       const filter = e.currentTarget;      
       e.dataTransfer.setData('text/plain', filter.getAttribute('id'));
@@ -397,6 +405,7 @@ const availableFilters = {
       element.classList.remove('drag-origin');
       draggedElement = null;
       clearDraggedOverClassesFromAllFilters();
+      prohibitAllFilterDrags();
     };
 
     const handleDrop = (filter) => (e) => {
