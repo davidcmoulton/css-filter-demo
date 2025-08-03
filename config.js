873630290e03659e7ca48ffa699dab39ef830941
David Moulton <davidmm844 [at] gmail.com>
2025-08-03 23:32:29 +0100

    Extract config to its own module

---
+++ config.js
@@ -0,0 +1,73 @@
+export const config = {
+  defaultImagePath: './images/chopper.jpeg',
+  keyCode: {
+    enter: 13,
+    up: 38,
+    down: 40
+  },
+  availableFilters: {
+    blur: {
+      min: 0,
+      max: 10,
+      step: 0.5,
+      unit: 'px',
+      initial: 0,
+    },
+    brightness: {
+      min: 0,
+      max: 200,
+      step: 1,
+      unit: '%',
+      initial: 100,
+    },
+    contrast: {
+      min: 0,
+      max: 200,
+      step: 1,
+      unit: '%',
+      initial: 100,
+    },
+    grayscale: {
+      min: 0,
+      max: 100,
+      step: 1,
+      unit: '%',
+      initial: 0,
+    },
+    'hue-rotate': {
+      min: 0,
+      max: 360,
+      step: 1,
+      unit: 'deg',
+      initial: 0,
+    },
+    invert: {
+      min: 0,
+      max: 100,
+      step: 1,
+      unit: '%',
+      initial: 0,
+    },
+    opacity: {
+      min: 0,
+      max: 100,
+      step: 1,
+      unit: '%',
+      initial: 100,
+    },
+    saturate: {
+      min: 0,
+      max: 200,
+      step: 1,
+      unit: '%',
+      initial: 100,
+    },
+    sepia: {
+      min: 0,
+      max: 100,
+      step: 1,
+      unit: '%',
+      initial: 0,
+    },
+  }
+};
