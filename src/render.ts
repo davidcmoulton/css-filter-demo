4450ab14d317b0e5c737b72f7f9b4b7b2e983480
David Moulton <davidmm844 [at] gmail.com>
2025-08-04 13:17:49 +0100

    Fix bug in buildElement that stopped attributes being set

--- src/render.ts
+++ src/render.ts
@@ -1,12 +1,12 @@
-  type BuildElement = (name: string, attributes: { [index: string]: string }, classes: Array<string>| null) => HTMLElement;
+  type BuildElement = (name: string, attributes: { [index: string]: string | number }, classes: Array<string>| null) => HTMLElement;
 
   export const buildElement: BuildElement = (name, attributes, classes) => {
     const element = document.createElement(name);
 
     Object.keys(attributes).forEach((attr) => {
       const attrValue = attributes[attr]
-      if (attrValue?.length) {
-        element.setAttribute(attr, attrValue); 
+      if (attrValue !== undefined && attrValue !== null) {
+        element.setAttribute(attr, attrValue.toString());
       }
     });
 
