decd90e15a838d345e4cd7e4a64c7f8290d5cbf4
David Moulton <davidmm844 [at] gmail.com>
2025-08-06 23:49:21 +0100

    Harmonise whitespace in render module code

--- src/render.ts
+++ src/render.ts
@@ -1,34 +1,32 @@
-  const setAttribute = (name: string, value: string, element: HTMLElement): HTMLElement => {
-      if (value !== undefined && value !== null) {
-        element.setAttribute(name, value);
-      }
-      return element;
-    }
-
-  const setElementAttributes = (element: HTMLElement, attributes: { [index: string]: string | number }): HTMLElement => {
-    Object.keys(attributes).forEach((attribute) => {
-      setAttribute(attribute, attributes[attribute]?.toString() ?? '', element);
-    });
-    return element;
-  };
-
-  type BuildElement = (
-    name: string,
-    attributes: { [index: string]: string | number },
-    classes?: Array<string>
-  ) => HTMLElement;
-
-  export const buildElement: BuildElement = (name, attributes, classes): HTMLElement => {
-    const element = document.createElement(name);
-    setElementAttributes(element, attributes);
-    classes?.forEach((cls: string) => {element.classList.add(cls)});
-    return element;
+const setAttribute = (name: string, value: string, element: HTMLElement): HTMLElement => {
+  if (value !== undefined && value !== null) {
+    element.setAttribute(name, value);
   }
+  return element;
+}
 
-    export const buildButton = (id: string, text: string, type: 'button' | 'reset'): HTMLButtonElement => {
-      const button = buildElement('button', { id, type }, ['button']) as HTMLButtonElement;
-      button.innerHTML = text;
-      return button;
-    }
-  
-  
+const setElementAttributes = (element: HTMLElement, attributes: { [index: string]: string | number }): HTMLElement => {
+  Object.keys(attributes).forEach((attribute) => {
+    setAttribute(attribute, attributes[attribute]?.toString() ?? '', element);
+  });
+  return element;
+};
+
+type BuildElement = (
+  name: string,
+  attributes: { [index: string]: string | number },
+  classes?: Array<string>
+) => HTMLElement;
+
+export const buildElement: BuildElement = (name, attributes, classes): HTMLElement => {
+  const element = document.createElement(name);
+  setElementAttributes(element, attributes);
+  classes?.forEach((className: string) => {element.classList.add(className)});
+  return element;
+}
+
+export const buildButton = (id: string, text: string, type: 'button' | 'reset'): HTMLButtonElement => {
+  const button = buildElement('button', { id, type }, ['button']) as HTMLButtonElement;
+  button.innerHTML = text;
+  return button;
+}
