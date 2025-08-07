9f1168d7c165ff16e53731142820bc308a4c16da
David Moulton <davidmm844 [at] gmail.com>
2025-08-08 00:20:45 +0100

    Reorder code in buildUserFilter to tell a clearer story

--- src/index.ts
+++ src/index.ts
@@ -67,24 +67,24 @@ import * as render from './render.js';
     const filter = render.buildElement('fieldset', { id: `filter_${filterName}` }, ['filter']) as HTMLFieldSetElement;
 
     const userFilterWrapper = render.buildElement('div', {}, ['filter__toggle']);
-    const userFilterLabel = render.buildElement('label', { for: filterName }, ['filter__label']);
+    userFilterWrapper.addEventListener('keydown', keyDownHandler(filter), true);
+
     const userFilterToggle = render.buildElement('input', { id: filterName, type: 'checkbox', name: 'filters', value: 'on' }, ['visually-hidden']);
     userFilterToggle.addEventListener('input', handleInput)
+    userFilterWrapper.appendChild(userFilterToggle);
 
-    userFilterWrapper.addEventListener('keydown', keyDownHandler(filter), true);
+    const userFilterLabel = render.buildElement('label', { for: filterName }, ['filter__label']);
+    userFilterLabel.appendChild(document.createTextNode(`${filterName}(`));
+    userFilterLabel.appendChild(render.buildElement('output', { id: `magnitudeReporter_${filterName}` }));
+    userFilterLabel.appendChild(document.createTextNode(')'));
+    userFilterWrapper.appendChild(userFilterLabel);
 
     const dragHandle = render.buildElement('button', { type: 'button' }, ['filter__drag_handle']);
     dragHandle.addEventListener('mousedown', () => { filter.setAttribute('draggable', 'true') });
     dragHandle.addEventListener('mouseup', () => { filter.removeAttribute('draggable') });
     dragHandle.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" data-filter-move-icon><path d="M0 0h24v24H0z" fill="none"/><path d="M10 9h4V6h3l-5-5-5 5h3v3zm-1 1H6V7l-5 5 5 5v-3h3v-4zm14 2l-5-5v3h-3v4h3v3l5-5zm-9 3h-4v3H7l5 5 5-5h-3v-3z"/></svg>';
-
-    filter.appendChild(userFilterWrapper);
-    userFilterWrapper.appendChild(userFilterToggle);
-    userFilterWrapper.appendChild(userFilterLabel);
     userFilterWrapper.appendChild(dragHandle);
-    userFilterLabel.appendChild(document.createTextNode(`${filterName}(`));
-    userFilterLabel.appendChild(render.buildElement('output', { id: `magnitudeReporter_${filterName}` }));
-    userFilterLabel.appendChild(document.createTextNode(')'));
+    filter.appendChild(userFilterWrapper);
 
     const updateHandler = (e: Event) => { update(image, filters, canvas); };
     const magitudeComponent = magnitude.buildComponent(filterName, filterConstraints, updateHandler, image, filters, canvas);
