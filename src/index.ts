4331e99b91548f7818438641adc8dc0a7f352992
David Moulton <davidmm844 [at] gmail.com>
2025-08-08 00:44:13 +0100

    Extract buildDragHandle and buildUserFilterWrapper to further work towards better encapsulation

--- src/index.ts
+++ src/index.ts
@@ -57,6 +57,32 @@ import * as render from './render.js';
     return userFilterLabel;
   };
 
+  const buildDragHandle = (filter: HTMLElement): HTMLButtonElement => {
+    const dragHandle = render.buildElement('button', { type: 'button' }, ['filter__drag_handle']) as HTMLButtonElement;
+    dragHandle.addEventListener('mousedown', () => { filter.setAttribute('draggable', 'true') });
+    dragHandle.addEventListener('mouseup', () => { filter.removeAttribute('draggable') });
+    dragHandle.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" data-filter-move-icon><path d="M0 0h24v24H0z" fill="none"/><path d="M10 9h4V6h3l-5-5-5 5h3v3zm-1 1H6V7l-5 5 5 5v-3h3v-4zm14 2l-5-5v3h-3v4h3v3l5-5zm-9 3h-4v3H7l5 5 5-5h-3v-3z"/></svg>';
+    return dragHandle;
+  };
+
+  type BuildUserFilterWrapper = (filterName: string, handleInput: render.EventListenerCallback, keyDownHandler: KeyDownHandler, filter: HTMLFieldSetElement) =>  HTMLElement;
+  const buildUserFilterWrapper: BuildUserFilterWrapper = (filterName, handleInput, keyDownHandler, filter) => {
+    const userFilterWrapper = render.buildElement('div', {}, ['filter__toggle']);
+    userFilterWrapper.addEventListener('keydown', keyDownHandler(filter), true);
+
+    const userFilterToggle = render.buildElement('input', { id: filterName, type: 'checkbox', name: 'filters', value: 'on' }, ['visually-hidden']);
+    userFilterToggle.addEventListener('input', handleInput)
+    userFilterWrapper.appendChild(userFilterToggle);
+
+    const userFilterLabel = buildUserFilterLabel(filterName);
+    userFilterWrapper.appendChild(userFilterLabel);
+
+    const dragHandle = buildDragHandle(filter);
+    userFilterWrapper.appendChild(dragHandle);
+
+    return userFilterWrapper;
+  };
+
   const buildUserFilter = (
     filterName: string,
     image: HTMLImageElement,
@@ -74,21 +100,7 @@ import * as render from './render.js';
 
     const filter = render.buildElement('fieldset', { id: `filter_${filterName}` }, ['filter']) as HTMLFieldSetElement;
 
-    const userFilterWrapper = render.buildElement('div', {}, ['filter__toggle']);
-    userFilterWrapper.addEventListener('keydown', keyDownHandler(filter), true);
-
-    const userFilterToggle = render.buildElement('input', { id: filterName, type: 'checkbox', name: 'filters', value: 'on' }, ['visually-hidden']);
-    userFilterToggle.addEventListener('input', handleInput)
-    userFilterWrapper.appendChild(userFilterToggle);
-
-    const userFilterLabel = buildUserFilterLabel(filterName);
-    userFilterWrapper.appendChild(userFilterLabel);
-
-    const dragHandle = render.buildElement('button', { type: 'button' }, ['filter__drag_handle']);
-    dragHandle.addEventListener('mousedown', () => { filter.setAttribute('draggable', 'true') });
-    dragHandle.addEventListener('mouseup', () => { filter.removeAttribute('draggable') });
-    dragHandle.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" data-filter-move-icon><path d="M0 0h24v24H0z" fill="none"/><path d="M10 9h4V6h3l-5-5-5 5h3v3zm-1 1H6V7l-5 5 5 5v-3h3v-4zm14 2l-5-5v3h-3v4h3v3l5-5zm-9 3h-4v3H7l5 5 5-5h-3v-3z"/></svg>';
-    userFilterWrapper.appendChild(dragHandle);
+    const userFilterWrapper = buildUserFilterWrapper(filterName, handleInput, keyDownHandler, filter);
     filter.appendChild(userFilterWrapper);
 
     const updateHandler = (e: Event) => { update(image, filters, canvas); };
